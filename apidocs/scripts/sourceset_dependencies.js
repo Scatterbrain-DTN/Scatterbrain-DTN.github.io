a9df4b4277c90d02a50f42fd370a1a1fab91fc19
Alex Ballmer <gnu3ra@riseup.net>
2021-08-16 03:11:09 -0400

    add autogenerated apidocs

---
+++ apidocs/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":scatterbrainSDK:dokkaHtml/androidTestRelease":[],":scatterbrainSDK:dokkaHtml/debug":[],":scatterbrainSDK:dokkaHtml/main":[],":scatterbrainSDK:dokkaHtml/release":[]}'
